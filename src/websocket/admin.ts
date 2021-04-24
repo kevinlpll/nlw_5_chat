98042bf0db7c6c8768ee71eaa7386f56d1cdef5a
Kevin Lopes Paschoal <kevinlp@live.com>
2021-04-24 10:43:44 -0300

    Implementada listagem de mensagens do admin

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -1,10 +1,24 @@
+import { Socket } from "socket.io"
 import { io } from "../http"
 import { ConnectionService } from "../services/ConnectionsService"
+import { MessagesService } from "../services/MessagesService"
 
-io.on("connect", async () => {
+io.on("connect", async (socket) => {
   const connectionService = new ConnectionService()
+  const messagesService = new MessagesService()
   
   
   const allConnectionsWithoutAdmin = await connectionService.findAllWithoutAdmin()
   io.emit("admin_list_all_users", allConnectionsWithoutAdmin)
+
+  socket.on("admin_list_messages_by_user", async (params,callback) =>{
+    const { user_id } = params
+
+    const allMessages = await messagesService.listByUser(user_id)
+
+    callback(allMessages)
+
+  })
 })
+
+
